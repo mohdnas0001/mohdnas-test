f838f99a5d674143e2b8fcd33fe8b90249b1ec64
mohdnas0001 <mohdnas0001@gmail.com>
2025-04-24 23:58:52 +0100

    push

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 const config = {
    content: [
       "./pages/**/*.{ts,tsx}",
-      "./components/**/*.{ts,tsx}",
+      "./Components/**/*.{ts,tsx}",
       "./app/**/*.{ts,tsx}",
       "./src/**/*.{ts,tsx}",
       "*.{js,ts,jsx,tsx,mdx}",
